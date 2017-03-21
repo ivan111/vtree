8bc316cae53ffa5408ea85b5cafc505b1bda4a85
ivan111 <uroyokoi@gmail.com>
2017-03-21 17:29:39 +0900

    remove commented out lines

--- src/decorator/linkName.js
+++ src/decorator/linkName.js
@@ -14,16 +14,10 @@ export default class LinkNameDecorator {
 
     const b = t.node().getBBox();
     const w = Math.ceil(b.width);
-    // const h = Math.ceil(b.height);
 
     const ww = w + pad * 2;
 
-    /*
-    const hh = h + pad * 2;
 
-    newG
-      .attr('transform', `translate(0,${-hh})`);
-    */
 
     return { width: Math.max(ww, width), height: height };
   }
